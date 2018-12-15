36c0b5507961cc67bbf2022c183175c58b7b3f06
marked <marked@users.noreply.github.com>
2018-12-15 04:58:15 -0600

    Add warning comment about GPDR test coverage

--- pipeline.py
+++ pipeline.py
@@ -169,7 +169,8 @@ def stats_id_function(item):
 class WgetArgs(object):
     def realize(self, item):
         if item['tumblr_requires_login' or 'tumblr_requires_gpdr']:
-            user_agent = BOT_UA
+            user_agent = BOT_UA  # Warning: if we change our GDPR strategy to using a GDPR cookie with browser UA, 
+                                 # login-required tests need to be done with that
         else:
             user_agent = BROWSER_UA
 
